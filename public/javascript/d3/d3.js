fca9ab3ac93a8980f26581d25f4e5abe3de6c1ca
Ian Henderson <ianwhen@gmail.com>
2014-05-22 15:51:32 -0700

    Can make static bar chart on /d3 view now.

--- public/javascript/d3/d3.js
+++ public/javascript/d3/d3.js
@@ -1,1 +1,18 @@
-d3.js
+var server = io.connect('/d3');
+
+$(document).ready(function() {
+
+  server.on('welcome', function(data) {
+    console.log("welcomed", data);
+  });
+
+  // server.on('motionData', function(data) {
+  //   initialize(data);
+  // });
+
+  server.on('audio', function(data) {
+    initialize(data);
+    console.log(data);
+  });
+
+});
