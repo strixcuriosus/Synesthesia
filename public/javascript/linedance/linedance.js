06ab75a0ec1a248d9d5ab00bdd189eda7809bb63
Ian Henderson <ianwhen@gmail.com>
2014-05-26 19:57:49 -0700

    lines are really long now

--- public/javascript/linedance/linedance.js
+++ public/javascript/linedance/linedance.js
@@ -7,44 +7,42 @@ server.on('welcome', function (data) {
 var width = Math.max(960, innerWidth), //640
     height = Math.max(500, innerHeight); //480
 
+var lineHeight = 10;
+
 var throttledUpdate = _.throttle(function(optiFlowData) {
-      // if(optiFlowData.zones.length > 1000) {
 
-        var circles = svg.selectAll('circle').data(optiFlowData.zones, function(d,i){return (d.x + "x" + d.y);});
+        var lines = svg.selectAll('line').data(optiFlowData.zones, function(d,i){return (d.x + "x" + d.y);});
 
-        circles//.data(optiFlowData.zones, function(d,i){return (d[i].x + "x" + d[i].y);})
-          .attr("r", 0)
-          .style("stroke-opacity", 1)
-          .attr("cx", function(d) {return d.x * width / 640;})
-          .attr("cy", function(d) {return d.y * height / 480;})
+        lines.style("stroke-opacity", 1)
           .transition()
           .duration(1500)
-          // .attr("r", function(d) { return Math.max(0.3, (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))));})
-          .attr("r", function(d) { return (3 * (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))));})
-          // .style("stroke", "green") //d3.hsl((i = (i + 1) % 360), 1, .5)
-          .style("stroke-opacity", 0);
-
-        circles.enter()
-          .append('circle')
-          // .attr('width', 10)
-          // .attr('height', 15)
-          .attr("cx", function(d) {return d.x * width / 640;})
-          .attr("cy", function(d) {return d.y * height / 480;})
-          .attr("r", 0)
-          .style("stroke", "green") //d3.hsl((i = (i + 1) % 360), 1, .5)
-          .style("stroke-opacity", 1)
+          .attr("x1", function(d) {return d.x * width / 640;})
+          .attr("y1", function(d) {return d.y * height / 480;})
+          .attr("x2", function(d) {return d.u*(d.x * width / 640);})
+          .attr("y2", function(d) {return d.v*(d.y * height / 480);})
           .transition()
           .duration(1500)
-          // .attr("r", function(d) { return Math.max(0.3, (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))));})
-          .attr("r", function(d) { return (3 * (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))));})
-          // .attr("r", 50)
+          .attr("x2", function(d) {return (d.x * width / 640);})
+          .attr("y2", function(d) {return (d.y * height / 480);})
+
+        lines.enter()
+          .append('line')
+          .attr("x1", function(d) {return d.x * width / 640;})
+          .attr("y1", function(d) {return d.y * height / 480;})
+          .attr("x2", function(d) {return d.x * width / 640;})
+          .attr("y2", function(d) {return (d.y * height / 480);})
+          .style("stroke", "green"); //d3.hsl((i = (i + 1) % 360), 1, .5)
+          // .style("stroke-opacity", 1)
+          // .transition()
+          // .duration(1500)
+          // .attr("r", function(d) { return (3 * (Math.sqrt(Math.pow(d.u, 2) + Math.pow(d.v, 2))));})
           // .style("stroke", "green") //d3.hsl((i = (i + 1) % 360), 1, .5)
-          .style("stroke-opacity", 0)
+          // .style("stroke-opacity", 0)
           // .remove();
 
 
 
-        // circles.exit()
+        // lines.exit()
         //  .transition()
         //  .duration(700)
         //  .remove();
@@ -65,23 +63,5 @@ server.on('optiFlowData', function(data){
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
-
-// svg.append("rect")
-//     .attr("width", width)
-//     .attr("height", height)
-//     .on(("ontouchstart" in document ? "touchmove" : "mousemove"), particle);
-
-// var circle = svg.selectAll("circle");
-//    .data(init)
-//    .enter()
-//    .append('circle')
-//    .attr("width", 10)
-//    .attr("height", 15)
-//    .attr("cx", 400)
-//    .attr("cy", 400)
-//    .attr("r", 5);
-
-// var particle = function (dataPoint) {
-//   console.log(dataPoint);
-  
+ 
 // };
